5892c1ad794fbe611e3884b53563853c530bf20a
Ken Jacob <kenjacob0606@gmail.com>
2025-05-19 15:30:04 +0100

    .

--- neural_network.py
+++ neural_network.py
@@ -20,7 +20,7 @@ trainSet, valSet = torch.utils.data.random_split(training_set, [trainSet_size, v
 
 #load train and validation set
 train_loader = DataLoader(trainSet, batch_size=50, shuffle=True)
-val_loader = DataLoader(valSet, batch_size=50, shuffle=True)
+val_loader = DataLoader(valSet, batch_size=50, shuffle=False)
 
 #load test set
 test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
@@ -31,10 +31,10 @@ test_loader = DataLoader(test_set, batch_size=50, shuffle=False)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3)
+        self.conv1 = nn.Conv2d(3, 16, 3,padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3)
-        self.fc1 = nn.Linear(32 * 6 *6, 120)
+        self.conv2 = nn.Conv2d(16, 32, 3,padding=1)
+        self.fc1 = nn.Linear(32 * 8 * 8, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -54,9 +54,9 @@ classifier = NeuralNetwork().to(device)
 #Set Training Parameters
 
 lossFn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.009)
+optimizer = torch.optim.Adam(classifier.parameters(), lr=0.01)
 
-epochs = 100
+epochs = 75
 losses=[]
 # try:
 for epoch in range(epochs):
