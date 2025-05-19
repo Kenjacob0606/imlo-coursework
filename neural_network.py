2939184443c23363035262ff490b9d29ffd64b56
Ken Jacob <kenjacob0606@gmail.com>
2025-05-19 20:05:04 +0100

    better

--- neural_network.py
+++ neural_network.py
@@ -19,34 +19,37 @@ valSet_size = len(training_set) - trainSet_size
 trainSet, valSet = torch.utils.data.random_split(training_set, [trainSet_size, valSet_size])
 
 #load train and validation set
-train_loader = DataLoader(trainSet, batch_size=50, shuffle=True)
-val_loader = DataLoader(valSet, batch_size=50, shuffle=False)
+train_loader = DataLoader(trainSet, batch_size=64, shuffle=True)
+val_loader = DataLoader(valSet, batch_size=64, shuffle=False)
 
 #load test set
 test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-test_loader = DataLoader(test_set, batch_size=50, shuffle=False)
+test_loader = DataLoader(test_set, batch_size=64, shuffle=False)
 
 #Neural Network Architecture
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3,padding=1)    #after conv becomes -> 16,32,32 ->pool -> 16,16,16
+        self.conv1 = nn.Conv2d(3, 16, 3,padding=1)    #after conv becomes -> 16,32,32
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3,padding=1)   #after conv becomes -> 32,16,16
-        self.conv3 = nn.Conv2d(32, 64, 3,padding=1)    #after conv becomes -> 64,16,16 ->pool -> 64,8,8
-        self.fc1 = nn.Linear(64 * 8 * 8, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-
+        self.dropout = nn.Dropout(0.3)
+        self.conv2 = nn.Conv2d(16, 32, 3,padding=1)   #after conv becomes -> 32,32,32 ->pool -> 32,16,16
+        self.conv3 = nn.Conv2d(32, 64, 3,padding=1)    #after conv becomes -> 64,16,16
+        self.conv4 = nn.Conv2d(64, 100, 3,padding=1)    #after conv becomes -> 100,16,16 ->pool -> 100,8,8
+        self.fc1 = nn.Linear(100 * 8 * 8, 120)
+        self.fc2 = nn.Linear(120, 10)
+        # self.fc3 = nn.Linear(84, 10)
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = F.relu(self.conv2(x))
-        x = self.pool(F.relu(self.conv3(x)))
+        x = F.relu(self.conv1(x))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = self.pool(F.relu(self.conv4(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.dropout(x)
+        # x = F.relu(self.fc2(x))
+        x = self.fc2(x)
         return x
 
 
@@ -56,9 +59,9 @@ classifier = NeuralNetwork().to(device)
 #Set Training Parameters
 
 lossFn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.0005)
+optimizer = torch.optim.Adam(classifier.parameters(), lr=0.0001)
 
-epochs = 35
+epochs = 80
 losses=[]
 # try:
 for epoch in range(epochs):
