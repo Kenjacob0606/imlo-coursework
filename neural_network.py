e27a279d42e0378901f63ef17edf7df4a4759d0f
Ken Jacob <kenjacob0606@gmail.com>
2025-05-19 16:12:44 +0100

    .

--- neural_network.py
+++ neural_network.py
@@ -31,16 +31,18 @@ test_loader = DataLoader(test_set, batch_size=50, shuffle=False)
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 16, 3,padding=1)
+        self.conv1 = nn.Conv2d(3, 16, 3,padding=1)    #after conv becomes -> 16,32,32 ->pool -> 16,16,16
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(16, 32, 3,padding=1)
-        self.fc1 = nn.Linear(32 * 8 * 8, 120)
+        self.conv2 = nn.Conv2d(16, 32, 3,padding=1)   #after conv becomes -> 32,16,16
+        self.conv3 = nn.Conv2d(32, 64, 3,padding=1)    #after conv becomes -> 64,16,16 ->pool -> 64,8,8
+        self.fc1 = nn.Linear(64 * 8 * 8, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv2(x))
+        x = self.pool(F.relu(self.conv3(x)))
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -54,7 +56,7 @@ classifier = NeuralNetwork().to(device)
 #Set Training Parameters
 
 lossFn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(classifier.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(classifier.parameters(), lr=0.001)
 
 epochs = 75
 losses=[]
